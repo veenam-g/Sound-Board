044a89a581222dd3a6c5d2ffb0f0991bbf5b5c37
lollisjosh <loljcpsc@gmail.com>
2024-12-09 19:26:15 -0800

    Add more imports

--- app/src/main/java/com/example/soundboard/FreesoundApi.kt
+++ app/src/main/java/com/example/soundboard/FreesoundApi.kt
@@ -3,6 +3,8 @@ package com.example.soundboard
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface FreesoundApi {
 
