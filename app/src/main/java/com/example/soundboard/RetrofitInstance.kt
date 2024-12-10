b7fc5a0a1c5e1c6ae43387c4b2b396bb970c46a3
lollisjosh <loljcpsc@gmail.com>
2024-12-09 21:18:08 -0800

    Add retrofitinstance.kt

--- app/src/main/java/com/example/soundboard/RetrofitInstance.kt
+++ app/src/main/java/com/example/soundboard/RetrofitInstance.kt
@@ -1,4 +1,17 @@
+// File: app/src/main/java/com/example/soundboard/RetrofitInstance.kt
 package com.example.soundboard
 
-class RetrofitInstance {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://freesound.org/apiv2/"
+
+    val api: FreesoundApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(FreesoundApi::class.java)
+    }
 }
