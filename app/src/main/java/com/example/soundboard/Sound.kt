56ef0bb3dd4725ef5415a2e04b0341d1cf419efa
lollisjosh <loljcpsc@gmail.com>
2024-12-09 21:33:42 -0800

    Add 5 sound buttons in landscape

--- app/src/main/java/com/example/soundboard/Sound.kt
+++ app/src/main/java/com/example/soundboard/Sound.kt
@@ -1,4 +1,9 @@
+// File: app/src/main/java/com/example/soundboard/Sound.kt
 package com.example.soundboard
 
-class Sound {
-}
+data class Sound(
+    val id: Int,
+    val url: String,
+    val name: String,
+    val previews: Map<String, String>
+)
