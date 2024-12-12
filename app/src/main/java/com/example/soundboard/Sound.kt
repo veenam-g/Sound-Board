5fb75f4432d2bedf1ce1db1aa5f9058a7d0b705f
HunterTran10 <hunter.q.tran@gmail.com>
2024-12-12 09:44:03 -0800

    Added logging to Sound.kt

--- app/src/main/java/com/example/soundboard/Sound.kt
+++ app/src/main/java/com/example/soundboard/Sound.kt
@@ -5,4 +5,15 @@ data class Sound(
     val url: String,
     val name: String,
     val previews: Map<String, String>
-)
+) {
+    init {
+        Log.d("Sound", "Created a sound object with ID: $id")
+        Log.i("Sound", "ID: $id, URL: $url, Name: $name, Previews: $previews")
+
+        if (url.isBlank()) {
+            Log.e("Sound", "URL is missing or empty for sound with ID: $id")
+        } else {
+            Log.w("Sound", "Make sure the URL $url is accessible for sound with ID: $id")
+        }
+    }
+}
